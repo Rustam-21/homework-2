569d01e10f46fc7a80469d745574b507b4d42502
Kalybaev Rustam <me@kalybaev.ru>
2023-04-22 15:14:26 +0500

    hw-task1

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число");
+string x = Console.ReadLine();
+int x1 = int.Parse(x);
+
+int second = x1 / 10 % 10;
+
+System.Console.WriteLine($"Вторая цифра {x} - {second}.");
