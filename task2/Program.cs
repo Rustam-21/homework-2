d2dcaf0a675895884d17831964d9d4476a120fa7
Kalybaev Rustam <me@kalybaev.ru>
2023-04-22 15:18:16 +0500

    hw-task2

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int number1 = int.Parse(number);
+
+int length = 1;
+int count = 10;
+
+if (number1 < 0)
+{
+    number1 = number1 * -1;
+}
+
+while (number1 / count > 0)
+{
+    length++;
+    count = count * 10;
+}
+System.Console.WriteLine($"Длина числа {length} символов.");
+
+if (length < 3)
+{
+    System.Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    count = count / 1000;
+    System.Console.WriteLine($"Третья цифра числа {number} - это {number1 / count % 10}");
+}
